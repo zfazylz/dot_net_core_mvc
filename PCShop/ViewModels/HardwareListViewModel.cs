7ea9d2f3a6b4cfa39014b47ef939fdf08c921770
Fazylzhan <faza.zheten123@gmail.com>
2020-04-23 21:44:22 +0600

    Views added

---
+++ PCShop/ViewModels/HardwareListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PCShop.Models;
+
+namespace PCShop.ViewModels
+{
+    public class HardwareListViewModel
+    {
+        public IEnumerable<Hardware> Hardwares { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
---
+++ PCShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PCShop.Models;
+
+namespace PCShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Hardware> HardwareOnSale { get; set; }
+    }
+}
